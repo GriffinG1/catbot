7a5e3089dfad327f96b932de278c5f13aa9f5aed
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-05-15 01:20:10 -0600

    Voting is done

--- modules/vote.py
+++ modules/vote.py
@@ -45,17 +45,19 @@ class Vote(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
+        self.messages = []
 
     async def make_embed(self, ctx, arr, index):
         embed = discord.Embed(description="")
         orig_index = index
         for mod in arr:
-            embed.description += f"{mod[0]} - {unicode_letters[index]}\n"
+            embed.description += f"{mod} - {unicode_letters[index]}\n"
             index += 1
         embed.set_footer(text=f"{len(self.bot.mod_dict)} total mods")
         vote_message = await ctx.send(embed=embed)
         for x in range(len(arr)):
             await vote_message.add_reaction(unicode_letters[x + orig_index])
+        self.messages.append(await ctx.channel.fetch_message(vote_message.id))
         
     @commands.command()
     async def create_vote(self, ctx):
@@ -65,6 +67,22 @@ class Vote(commands.Cog):
             await self.make_embed(ctx, dict[:20], index)
             del dict[:20]
             index += 20
+
+    @commands.command()
+    async def tally(self, ctx):
+        if len(self.messages) == 0:
+            return await ctx.send("You need to create a vote first!")
+        embed = discord.Embed(description="")
+        index = 0
+        for message in self.messages:
+            message = await ctx.channel.fetch_message(message.id)
+            for reaction in message.reactions:
+                if reaction.count >= 3:
+                    embed.description += f"**{self.bot.mod_dict[message.reactions.index(reaction) + index]} - {reaction.count-1}**\n"
+                else:
+                    embed.description += f"{self.bot.mod_dict[message.reactions.index(reaction) + index]} - {reaction.count-1}\n"
+            index += 20
+        await ctx.send(embed=embed)
 
     
 def setup(bot):
